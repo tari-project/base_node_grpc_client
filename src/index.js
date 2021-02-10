fc9282140e61a8b0026d293ec4e58d9a5dc5fe0d
Mike Berry <mikethetike@tari.com>
2021-02-10 18:11:12 +0200

    Add chain metadata

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ function connect(address) {
 function Client(address) {
     this.inner = connect(address);
 
-   ['getVersion', 'listHeaders', 'getBlocks', 'getMempoolTransactions'].forEach((method) => {
+   ['getVersion', 'listHeaders', 'getBlocks', 'getMempoolTransactions', 'getChainMetdata'].forEach((method) => {
         this[method] = (arg) => this.inner[method]().sendMessage(arg);
     })
 }
