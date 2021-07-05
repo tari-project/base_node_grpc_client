bea91a97657d27fda497c34aa547eb5922ddf656
Mike the Tike <mikethetike@tari.com>
2021-07-05 14:07:19 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,36 +1,47 @@
-const grpc = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader');
-const {promisifyAll} = require("grpc-promise");
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const { promisifyAll } = require("grpc-promise");
 
 const packageDefinition = protoLoader.loadSync(
-    `${__dirname}/../protos/base_node.proto`,
-    {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
-    });
+  `${__dirname}/../protos/base_node.proto`,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  }
+);
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 const tariGrpc = protoDescriptor.tari.rpc;
 
 function connect(address) {
-    const client = new tariGrpc.BaseNode(address, grpc.credentials.createInsecure());
-    promisifyAll(client, {metadata: new grpc.Metadata()});
-    return client;
+  const client = new tariGrpc.BaseNode(
+    address,
+    grpc.credentials.createInsecure()
+  );
+  promisifyAll(client, { metadata: new grpc.Metadata() });
+  return client;
 }
 
 function Client(address) {
-    this.inner = connect(address);
+  this.inner = connect(address);
 
-   ['getVersion', 'listHeaders', 'getBlocks', 'getMempoolTransactions', 'getTipInfo'].forEach((method) => {
-        this[method] = (arg) => this.inner[method]().sendMessage(arg);
-    })
+  [
+    "getVersion",
+    "listHeaders",
+    "getBlocks",
+    "getMempoolTransactions",
+    "getTipInfo",
+    "getTokens",
+  ].forEach((method) => {
+    this[method] = (arg) => this.inner[method]().sendMessage(arg);
+  });
 }
 
-Client.connect = (address) => new Client(address)
+Client.connect = (address) => new Client(address);
 
 module.exports = {
-    Client,
-    types: tariGrpc,
+  Client,
+  types: tariGrpc,
 };
